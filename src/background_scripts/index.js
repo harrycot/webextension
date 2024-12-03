2653c15878c14cf3518e2ea0aead93e5d5d9b913
Florian Blanc <hvert890@gmail.com>
2024-12-03 10:06:25 +0000

    populate identities to dropdown

--- src/background_scripts/index.js
+++ src/background_scripts/index.js
@@ -4,7 +4,6 @@ const openpgp = require('openpgp');
 // service_worker: https://codimite.ai/blog/service-workers-in-chrome-extensions-mv3-powering-background-functionality/
 
 browser.runtime.onInstalled.addListener(async () => {
-    // have to set a master password for encryption
     await browser.storage.local.set({ id_array: [] });
 });
 
@@ -15,11 +14,11 @@ browser.runtime.onMessage.addListener(
             const _storage_response = await browser.storage.local.get("id_array");
             _storage_response.id_array.push({ name: request.data.name, email: request.data.email, keys: _id_keys });
             await browser.storage.local.set({ id_array: _storage_response.id_array });
-            browser.runtime.sendMessage({info: "new_identity", id: {name: request.data.name, email: request.data.email} });
+            browser.runtime.sendMessage(Object.assign(request, {response: {name: request.data.name, email: request.data.email} }));
         } else if (request.action === "get_identities") {
             const _storage_response = await browser.storage.local.get("id_array");
             const _identities_view = _storage_response.id_array.map((id) => { return { name: id.name, email: id.email } });
-            browser.runtime.sendMessage({ info: "get_identities", data: {id_array: _identities_view} });
+            browser.runtime.sendMessage(Object.assign(request, {response: {id_array: _identities_view} }));
         }
     }
 );
--- src/popup/index.js
+++ src/popup/index.js
@@ -5,9 +5,16 @@ uikit.use(uikit_icons);
 
 // use async or promise : https://github.com/mozilla/webextension-polyfill?tab=readme-ov-file#examples
 
+const new_identity_html = (name) => {
+    const _identities = document.getElementById('identities');
+    const _li = document.createElement('li');
+        _li.innerHTML = `<a href="/identity/${name}"><span class="uk-margin-small-right" uk-icon="icon: user"></span>${name}</a>`
+    _identities.appendChild(_li);
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     const _new_identity = document.getElementById('new_identity');
-    browser.runtime.sendMessage({ action: "get_identities" });
+    browser.runtime.sendMessage({ action: "get_identities", tag: "init" });
     // if no identity draw new else 
     
     _new_identity.addEventListener('click', async () => {
@@ -18,11 +25,20 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     browser.runtime.onMessage.addListener(
         async (request, sender, sendResponse) => {
-            if (request.info === "new_identity") {
-                console.log(request.id);
-                browser.runtime.sendMessage({ action: "get_identities" });
-            } else if (request.info === "get_identities") {
-                console.log(request.data.id_array);
+            if (request.response) {
+                if (request.action === "new_identity") {
+                    new_identity_html(request.response.name);
+                    console.log(request.response);
+                    browser.runtime.sendMessage({ action: "get_identities" });
+                } else if (request.action === "get_identities") {
+                    console.log(request.response.id_array);
+
+                    if (request.tag === "init") {
+                        for (const id of request.response.id_array) {
+                            new_identity_html(id.name);
+                        }
+                    }
+                }
             }
         }
     );
